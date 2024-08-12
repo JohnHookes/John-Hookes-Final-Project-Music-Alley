21ec79c359f045c78d045c68720864ac3c627b0d
John Hooks <johnhookes@aol.com>
2024-08-12 08:34:21 +0000

    collectstatic and set debug to false for deploy

--- artists/forms.py
+++ artists/forms.py
@@ -1,4 +1,5 @@
 from .models import Comment
+from .models import Post
 from django import forms
 
 
@@ -6,3 +7,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('artist_name' 'about', 'artist_name', 'genre',)
+
